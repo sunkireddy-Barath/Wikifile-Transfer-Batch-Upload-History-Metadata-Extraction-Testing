7b2d3632dbcb94162a931ec5caeea64216b9e7b8
Sunkireddy Barath <sunkireddybarath07@gmail.com>
2026-03-31 15:34:27 +0530

    chore/add-setup-and-start-automation-scripts

---
+++ wikifile-transfer-frontend/setup.bat
@@ -0,0 +1,8 @@
+@echo off
+echo ==========================================
+echo Wikifile-Transfer Frontend Setup
+echo ==========================================
+echo Starting installation of dependencies...
+npm install --force
+echo Installation complete!
+pause
---
+++ wikifile-transfer-frontend/start.bat
@@ -0,0 +1,7 @@
+@echo off
+echo ==========================================
+echo Starting Wikifile-Transfer Frontend (MOCK MODE)
+echo ==========================================
+set VITE_USE_MOCK=true
+npm run dev
+pause
